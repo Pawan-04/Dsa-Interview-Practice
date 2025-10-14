d237636611e7cdc7142b2ba9fee0abdec0afbf22
Pawan Kumar Pandey <pawan4kp@gmail.com>
2025-10-14 18:27:38 +0530

    Selection sort

---
+++ Random/1-LinearWasSimillarTo-Selection.js
@@ -0,0 +1,19 @@
+let arr = [0,1,2,1]
+
+let sort = (arr)=>{
+    for(let i=0;i<arr.length-1;i++){
+        let min = i
+        for(let j=i+1;j<arr.length;j++){
+            if(arr[j]<arr[min]){
+                min=j
+            }
+        }
+        let temp = arr[i];
+        arr[i] = arr[min]
+        arr[min] = temp;
+    }
+
+    return arr
+}
+
+console.log( sort(arr) );
